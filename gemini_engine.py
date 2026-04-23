d7744f38ffb2d015ce38fbfb940aa9aec02a1315
bimka183 <bimka183@gmail.com>
2026-04-24 02:04:16 +0300

    Adding saving and deleting context

--- gemini_engine.py
+++ gemini_engine.py
@@ -1,4 +1,3 @@
-
 from config import GEMINI_API_KEY, PROXY_URL
 from google import genai
 from google.genai import types
@@ -9,11 +8,17 @@ http_options = types.HttpOptions(
 )
 
 client = genai.Client(api_key=GEMINI_API_KEY, http_options=http_options)
+context = dict()
 
-async def my_ai(query):
-    response = await client.aio.models.generate_content(
-        model='gemini-3.1-flash-lite-preview', # Модель можете заменить. Список моделей находятся по ссылке:
-        contents = query
-    )
+async def my_ai(query, id):
+    if id not in context:
+        context.update({id: []})
+    chat = client.aio.chats.create(model='gemini-3.1-flash-lite-preview', history=context[id]) # Модель можете заменить. Список моделей находятся по ссылке:
+    content = await chat.send_message(query)
+    context[id] = chat.get_history()
     print('Запрос выполнен')
-    return response.text
+    return content.text
+
+async def delete_chat(id):
+    context.pop(id, None)
+    return "Чат удалён"
--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 import asyncio
 
-from gemini_engine import my_ai
+from gemini_engine import my_ai, delete_chat
 
 from vkbottle import API, Bot, user
+from vkbottle import Keyboard, KeyboardButtonColor, Text
 from vkbottle.bot import Message
 from config import VK_TOKEN
 bot = Bot(token = VK_TOKEN)
+
+keyboard = Keyboard(one_time=True, inline=False)
+keyboard.add(Text("Удалить чат"), color=KeyboardButtonColor.NEGATIVE)
+keyboard=keyboard.get_json()
+
+@bot.on.message(text="Удалить чат")
+async def Delete_history(message: Message):
+    user_id = message.from_id
+    await delete_chat(user_id)
+    await message.answer("Контекст очищен!", keyboard=keyboard)
 
 @bot.on.message(text="Тест") #Правило для хендлера ЛИЧНЫХ сообщений
 async def test_handler(message:Message):
     await message.answer("Тест пройден!")
 
+
 @bot.on.message() # Если вводить что-то, что не выполняет все остальные правила, то будет работать эта функция
-async def anyway_handler(message:Message):
-    users_info = await bot.api.users.get(user_ids=[message.from_id])
-    user = users_info[0]
+async def anyway_handler(message: Message):
+    user_id = message.from_id
     text = message.text
-    await message.answer(await my_ai(text))
+    ai_response = await my_ai(text, user_id)
+    print(ai_response)
+    await message.answer(ai_response, keyboard=keyboard)
 
 bot.run_forever()
